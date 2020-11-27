fa90ce2d24aacf0a474323fbc1b141cd952c1b97
nkradebe <nkradebe@student.wethinkcode.co.za>
2020-11-27 10:09:45 +0200

    student login

---
+++ student_info.py
@@ -0,0 +1,10 @@
+def login_details():
+    user_name = input('Please enter your username:')
+    email = input('Please enter your email:')
+    password = input('please enter a password:')    # print(user_name)
+    # print(email)    
+    email_file = open("email.txt", "a")
+    email_file.write(user_name+'\n'+email+'\n'+password+'\n\n')
+    email_file.close()
+    # print(email_file)
+    return email_file
